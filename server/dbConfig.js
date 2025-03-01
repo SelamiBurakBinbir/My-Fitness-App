ae63e9aa4d35b8f385cf53546124ea329c23b096
SelamiBurakBinbir <sbbinbir@gmail.com>
2025-03-02 01:07:22 +0300

    Update dbConfig.js

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -4,13 +4,22 @@
  ************************************************************/
 const { Pool } = require("pg");
 
+const isProduction = process.env.DATABASE_URL ? true : false;
+
 // Çevre değişkenlerinden veya sabit değerlerden okunabilir.
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "fitnessdb",
-  password: "1001",
-  port: 5432,
-});
+const config = {
+  user: process.env.DB_USER || "postgres",
+  host: process.env.DB_HOST || "localhost",
+  database: process.env.DB_NAME || "fitnessdb",
+  password: process.env.DB_PASSWORD || "1001",
+  port: parseInt(process.env.DB_PORT || "5432", 10),
+  // Eğer production ortamındaysanız, connectionString ve ssl ayarlarını ekleyin.
+  ...(isProduction && {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  }),
+};
+
+const pool = new Pool(config);
 
 module.exports = pool;
